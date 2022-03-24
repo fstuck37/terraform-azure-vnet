3d17c80bd332f14727518b3fff20784e58fd59df
Stuck, Fred <StuckF@DNB.com>
2022-03-24 15:45:25 -0400

    fix tcp and net watcher

--- network_security_group.tf
+++ network_security_group.tf
@@ -8,17 +8,17 @@ resource "azurerm_network_security_group" "security_groups" {
 
 resource "azurerm_network_security_rule" "lb-in-4095" {
   for_each = { for k, v in var.subnets: k => v if k != var.gatewaysubnet_subnet_name && k != var.public_subnet_name && var.default_deny_all && !contains(keys(var.set_subnet_specific_delegation), k)}
-    name                        = "AllowAzureLoadBalancerInBound-TCP"
-    priority                    = 4095
-    direction                   = "Inbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_address_prefix       = "AzureLoadBalancer"
-    source_port_range           = "*"
-    destination_address_prefix  = azurerm_subnet.subnets[each.key].address_prefix
-    destination_port_range      = "*"
-    resource_group_name         = azurerm_resource_group.rg-vnet.name
-    network_security_group_name = azurerm_network_security_group.security_groups[each.key].name
+    name                          = "AllowAzureLoadBalancerInBound-TCP"
+    priority                      = 4095
+    direction                     = "Inbound"
+    access                        = "Allow"
+    protocol                      = "Tcp"
+    source_address_prefix         = "AzureLoadBalancer"
+    source_port_range             = "*"
+    destination_address_prefixes  = azurerm_subnet.subnets[each.key].address_prefixes
+    destination_port_range        = "*"
+    resource_group_name           = azurerm_resource_group.rg-vnet.name
+    network_security_group_name   = azurerm_network_security_group.security_groups[each.key].name
 }
 
 resource "azurerm_network_security_rule" "DenyAllInBound" {
