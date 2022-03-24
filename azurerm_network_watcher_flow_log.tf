0c6e703004a0f0089b8484c6afa114ffd941f468
Stuck, Fred <StuckF@DNB.com>
2022-03-24 16:03:10 -0400

    fix tcp and net watcher

--- azurerm_network_watcher_flow_log.tf
+++ azurerm_network_watcher_flow_log.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_watcher_flow_log" "flow-log" {
   for_each = { for k, v in var.subnets: k => v if k != var.gatewaysubnet_subnet_name && var.log-storage-account != "" && var.network_watcher_name != "" && var.network_watcher_resource_group_name !="" && !contains(keys(var.set_subnet_specific_delegation), k) }
-    name                      = var.network_watcher_name
+    name                      = var.vnet-name == "" ? "Microsoft.Networkrg-${var.name-vars["account"]}-${var.region}-${var.name-vars["name"]}" : "Microsoft.Networkrg-${var.vnet-name}"
     network_watcher_name      = var.network_watcher_name
     resource_group_name       = var.network_watcher_resource_group_name
     network_security_group_id = azurerm_network_security_group.security_groups[each.key].id
